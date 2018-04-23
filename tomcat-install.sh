235fc1378b0819838a9bfbbdbba9d3dd42017da1
nisanthjs <nisanth_js@infosys.com>
2018-04-23 13:58:07 +0530

    Update tomcat-install.sh

--- tomcat-install.sh
+++ tomcat-install.sh
@@ -9,12 +9,12 @@ sudo apt-get install -y  $2
 
 if netstat -tulpen | grep 8080
 then
-    sudo wget http://mastervm.centralus.cloudapp.azure.com:8081/nexus/content/repositories/Silos_Repo/com/devops/azure/silos/1.0.0.5/silos-1.0.0.5.war -O /tmp/silos-1.0.0.5.war
+    sudo wget http://mastervm.centralus.cloudapp.azure.com:8081/nexus/content/repositories/Silos_Repo/com/devops/azure/silos/1.0.0.5/silos-1.0.0.5.war -O /tmp/silos.war
 	webapps_dir=/var/lib/tomcat7/webapps
 	# Remove existing assets (if any)
 	sudo rm -rf $webapps_dir/ROOT
 	# Copy WAR file into place
-	sudo cp /tmp/silos-1.0.0.5.war $webapps_dir
+	sudo cp /tmp/silos.war $webapps_dir
 	# Restart tomcat
 	sudo /etc/init.d/tomcat7 restart
 	exit 0
