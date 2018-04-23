b123dd1219d30388a7a1f197a49149610003add5
nisanthjs <nisanth_js@infosys.com>
2018-04-23 17:09:48 +0530

    Update install_tomcat_mysql.sh

--- install_tomcat_mysql.sh
+++ install_tomcat_mysql.sh
@@ -1,26 +1,10 @@
 java_config=$1
 tomcat_config=$2
-mysqlPassword=$3
+MYSQL_ROOT_PASSWORD=$3
 
-sudo apt-get update
-#no password prompt while installing mysql server
-#export DEBIAN_FRONTEND=noninteractive
-
-#another way of installing mysql server in a Non-Interactive mode
-echo "mysql-server-5.6 mysql-server/root_password password $mysqlPassword" | sudo debconf-set-selections 
-echo "mysql-server-5.6 mysql-server/root_password_again password $mysqlPassword" | sudo debconf-set-selections 
-
-#install mysql-server 5.6
-sudo apt-get -y install mysql-server-5.6
-
-#set the password
-#sudo mysqladmin -u root password "$mysqlPassword"   #without -p means here the initial password is empty
-
-#alternative update mysql root password method
-sudo mysql -u root -e "set password for 'root'@'localhost' = PASSWORD('$mysqlPassword')"
-#without -p here means the initial password is empty
-
-#sudo service mysql restart
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
 
 # Install Java
 sudo apt-get -y update
